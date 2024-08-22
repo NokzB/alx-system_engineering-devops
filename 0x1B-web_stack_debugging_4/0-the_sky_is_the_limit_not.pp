d4cd7abe763da6577f1adb07dec9de27c6571d17
Nokuphila <nnblb58@gmail.com>
2024-08-22 09:19:33 +0000

    code for debugging project

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,23 @@
+# code to correct the problem of a high amount of requests
+
+
+
+exec {'replace':
+
+  provider => shell,
+
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+
+  before   => Exec['restart'],
+
+}
+
+
+
+exec {'restart':
+
+  provider => shell,
+
+  command  => 'sudo service nginx restart',
+
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,1 @@
+This repo contains the code used to debug 0x1B-web_stack_debugging_4
